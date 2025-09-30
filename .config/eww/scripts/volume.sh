ac3b5a9c5cdd977c3f57fa4cd765fcf062eb84a5
JoelVCrasta <loejstarc@gmail.com>
2025-09-30 16:48:58 +0530

    updated for new install

--- .config/eww/scripts/volume.sh
+++ .config/eww/scripts/volume.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-
--- .config/waybar/scripts/cava.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cava | while read -r line; do
-  bars=$(echo "$line" | tr -d '[],' | tr -s ' ')
-  echo "{\"text\": \"$bars\", \"class\": \"cava\"}"
-done
